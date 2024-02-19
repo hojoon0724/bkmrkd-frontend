7ab6bb92e20420b92be75126cf395a7ac43a446e
Hojoon Kim <hojoon0724@gmail.com>
2024-02-18 22:13:46 -0800

    login page placeholder text added and styled

--- src/components/auth.js
+++ src/components/auth.js
@@ -2,8 +2,8 @@ function Auth() {
   return (
     <div className="login-form-container">
       <form>
-        <input type="text" name="username" id="username" />
-        <input type="password" name="password" id="password" />
+        <input type="text" name="username" id="username" placeholder="username" />
+        <input type="password" name="password" id="password" placeholder="password" />
         <div className="login-form-buttons">
           <input type="submit" value="Sign Up" />
           <input type="submit" value="Login" />
