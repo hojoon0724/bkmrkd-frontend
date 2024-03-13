052ca53ecb6d5efe1b32b81f7e4dcee7be817b82
hojoon0724 <hojoon0724@gmail.com>
2024-03-13 14:12:27 -0700

    add component always visible

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,14 @@
 import Logo from './icons/logo';
-import MobileAddNew from './header/mobileAddnew';
-import NewEntryBtn from './header/newEntryBtn';
 import Hello from './header/hello';
 import LogoutBtn from './header/logoutBtn';
 
 function Header({ onButtonClick }) {
   return (
     <div className="header">
-      <MobileAddNew />
       <ul className="nav__menu">
         <div className="header-container">
           <div className="header-top-section">
             <Logo />
-
-            <NewEntryBtn action={onButtonClick} />
           </div>
           <div className="header-bottom-section">
             <Hello />
--- src/pages/bookmarks.js
+++ src/pages/bookmarks.js
@@ -2,21 +2,15 @@ import Header from '../components/header';
 import BookmarkCard from '../components/bookmarkCard';
 import BookmarkForm from '../components/bookmarkForm';
 import { useLoaderData } from 'react-router-dom';
-import { useState } from 'react';
 
 function Bookmarks() {
   const data = useLoaderData();
-  const [newBookmark, setNewBookmark] = useState('');
-
-  function handleNewBookmark() {
-    setNewBookmark(<BookmarkForm />);
-  }
 
   return (
     <div className="top-container">
-      <Header onButtonClick={handleNewBookmark} />
+      <Header />
       <div className="bookmarks-container">
-        {newBookmark}
+        <BookmarkForm />
         {data.map((bookmarkData) => {
           return <BookmarkCard key={bookmarkData._id} data={bookmarkData} />;
         })}
